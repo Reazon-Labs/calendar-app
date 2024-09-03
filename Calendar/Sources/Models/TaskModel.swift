f073b5f900ca23f861b15836089843a668852d94
NotAJocke <jockerider199@gmail.com>
2024-09-03 08:38:35 +0200

    feat: task struct
    closes #2

---
+++ Calendar/Sources/Models/TaskModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct TaskModel {
+    var title: String
+    var project: String
+    var deadline: Date?
+    var estimatedTime: Int = 30
+    var priority: TaskPriority = .None
+    var doDates: [Date]
+    var body: String?
+    var isProject: Bool = false
+    var subtasks: [TaskModel]
+    var done: Bool = false
+}
+
+enum TaskPriority {
+    case None
+    case Low
+    case Medium
+    case High
+}
